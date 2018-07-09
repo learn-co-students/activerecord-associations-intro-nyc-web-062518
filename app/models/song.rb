d3393436c38f97aa91234f5e9743ff1adb3c7501
yunglleung1 <yungl.leung1@gmail.com>
2018-07-09 17:27:58 -0400

    Done.

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,3 +1,6 @@
 class Song < ActiveRecord::Base
-
+  belongs_to :artist
+  belongs_to :genre
+  # tells Song class that it will produce objects that can belong to an artist & genre
+  # (belongs_to macro)
 end
